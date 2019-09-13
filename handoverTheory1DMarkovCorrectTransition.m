ae0af0f4e85bc5c347490d2f7cc1e708ebb4ffb0
ThanosKou <koutsaftis.t@gmail.com>
2019-09-13 14:49:31 -0400

    Corrected some things

--- handoverTheory1DMarkovCorrectTransition.m
+++ handoverTheory1DMarkovCorrectTransition.m
@@ -8,20 +8,27 @@ ht = 5;%height BS
 frac = (hb-hr)/(ht-hr); %fraction depends on heights
 blocker_density = 0.1;
 expected_arr= 4*V*R*blocker_density*frac/(pi*3);
-deltaT=0.5;
+deltaT=0.002;
 Pbl = expected_arr/(expected_arr + mu);
 Pub = 1 - Pbl;
 aDt = expected_arr*deltaT;
 connectivity=1:1:5;
+M = 12;
 for k=1:length(connectivity)
     tranMatrix=zeros(connectivity(k)+1,connectivity(k)+1);
     K = connectivity(k); %maximum number of BSs for this connectivity
     for i=1:connectivity(k)+1
-        tranMatrix(i,i) = (1-(K-i-1)*aDt)*Pbl^(i-1);
+        Pb_updated = (Pbl*(M-connectivity)+1*i)/(M-connectivity+i);
+        Pub_updated= 1-Pb_updated;
+        tranMatrix(i,i) = (1-(K-i+1)*aDt)*Pb_updated^(i-1);
         if i<connectivity(k)+1
-            tranMatrix(i,i+1) = (K-i-1)*aDt;
+            tranMatrix(i,i+1) = (K-i+1)*aDt;
             for j=1:i
-                tranMatrix(i,j) = Pbl^(j-1)*Pub^(i-j)*(1-(K-i-1)*aDt);
+                tranMatrix(i,j) = nchoosek(i-1,i-j)*Pb_updated^(j-1)*Pub_updated^(i-j)*(1-(K-i+1)*aDt);
+            end  
+        else
+            for j=1:i
+                tranMatrix(i,j) = nchoosek(i-1,i-j)*Pb_updated^(j-1)*Pub_updated^(i-j)*(1-(K-i+1)*aDt);
             end  
         end 
         
