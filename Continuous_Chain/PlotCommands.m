02d8e741e4cc4ceebdc7f9c7c4ed5fe2f048e48b
mustafafu <furkanozkoc@gmail.com>
2020-01-17 18:15:57 -0500

    Change in self blockage for continous chain

--- Continuous_Chain/PlotCommands.m
+++ Continuous_Chain/PlotCommands.m
@@ -7,3 +7,55 @@ semilogy(lambda_BS,P_OS(:,1,1,2,2),'-r+')
 semilogy(lambda_BS,P_OS(:,1,1,5,1),'-bx')
 hold on;
 semilogy(lambda_BS,P_OS(:,1,1,5,2),'-b+')
+
+
+figure()
+semilogy(lambda_BS,P_OS(:,1,2,1,2),'-rx')
+grid on;
+hold on;
+semilogy(lambda_BS,P_OS(:,1,2,2,2),'-r+')
+semilogy(lambda_BS,P_OS(:,1,2,3,2),'-r*')
+semilogy(lambda_BS,P_OS(:,1,2,4,2),'-rd')
+semilogy(lambda_BS,P_OS(:,1,2,5,2),'-r^')
+
+% semilogy(lambda_BS,P_OS(:,1,2,1,2),'-bx')
+% semilogy(lambda_BS,P_OS(:,1,2,2,2),'-b+')
+% semilogy(lambda_BS,P_OS(:,1,2,3,2),'-b*')
+% semilogy(lambda_BS,P_OS(:,1,2,4,2),'-bd')
+% semilogy(lambda_BS,P_OS(:,1,2,5,2),'-b^')
+
+semilogy(lambda_BS,P_OS(:,2,2,1,2),'-gx')
+semilogy(lambda_BS,P_OS(:,2,2,2,2),'-g+')
+semilogy(lambda_BS,P_OS(:,2,2,3,2),'-g*')
+semilogy(lambda_BS,P_OS(:,2,2,4,2),'-gd')
+semilogy(lambda_BS,P_OS(:,2,2,5,2),'-g^')
+
+semilogy(lambda_BS,P_OS(:,3,2,1,2),'-mx')
+semilogy(lambda_BS,P_OS(:,3,2,2,2),'-m+')
+semilogy(lambda_BS,P_OS(:,3,2,3,2),'-m*')
+semilogy(lambda_BS,P_OS(:,3,2,4,2),'-md')
+semilogy(lambda_BS,P_OS(:,3,2,5,2),'-m^')
+
+semilogy(lambda_BS,P_OS(:,4,2,1,2),'-bx')
+semilogy(lambda_BS,P_OS(:,4,2,2,2),'-b+')
+semilogy(lambda_BS,P_OS(:,4,2,3,2),'-b*')
+semilogy(lambda_BS,P_OS(:,4,2,4,2),'-bd')
+semilogy(lambda_BS,P_OS(:,4,2,5,2),'-b^')
+
+
+
+
+figure()
+semilogy(K_list,P_OS(3,:,2,1,2),'-rx')
+hold on;
+semilogy(K_list,P_OS(3,:,2,2,2),'-r+')
+semilogy(K_list,P_OS(3,:,2,3,2),'-r*')
+semilogy(K_list,P_OS(3,:,2,4,2),'-rd')
+semilogy(K_list,P_OS(3,:,2,5,2),'-r^')
+grid on;
+
+semilogy(K_list,P_OS(3,:,2,1,1),'-bx')
+semilogy(K_list,P_OS(3,:,2,2,1),'-b+')
+semilogy(K_list,P_OS(3,:,2,3,1),'-b*')
+semilogy(K_list,P_OS(3,:,2,4,1),'-bd')
+semilogy(K_list,P_OS(3,:,2,5,1),'-b^')
