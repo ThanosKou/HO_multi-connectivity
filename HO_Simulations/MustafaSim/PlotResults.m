5eab1077b9a05ab8685898fed12a5cb70f4254a5
mustafafu <furkanozkoc@gmail.com>
2020-02-26 16:36:10 -0500

    Update plot script

--- HO_Simulations/MustafaSim/PlotResults.m
+++ HO_Simulations/MustafaSim/PlotResults.m
@@ -6,27 +6,35 @@ lambda_BS = [200 300 400 500]*10^(-6);
 connectivity = [1 2 3 4];
 
 load('finalresults.mat')
+% load('finalresults_old:(.mat')
+% load('finalresults_no_self_blockage.mat')
+% load('finalresults_expTimers.mat')
 load('NoRLF_Numerical-Results.mat')
 
 D_BL =2;
-dt = 4;
+dt = 3;
 w=1;
 figure()
 
-K_1=4;
+K_1=3;
 semilogy(lambda_BS, reshape(final_results(dt,w,:,K_1,D_BL),[],1),'-+r')
 hold on;
 grid on;
 semilogy(lambda_BS,P_OS(:,K_1,w,dt,D_BL),'-b+')
+
+% K_2=2;
+% semilogy(lambda_BS, reshape(final_results(dt,w,:,K_2,D_BL),[],1),'-^r')
+% semilogy(lambda_BS,P_OS(:,K_2,w,dt,D_BL),'-b^')
+
 
 K_2=1;
 semilogy(lambda_BS, reshape(final_results(dt,w,:,K_2,D_BL),[],1),'-vr')
 semilogy(lambda_BS,P_OS(:,K_2,w,dt,D_BL),'-bv')
 
 legend(['Simulation K=',num2str(K_1)],['Theory K=',num2str(K_1)],['Simulation K=',num2str(K_2)],['Theory K=',num2str(K_2)])
-title(['High Blocker Density w= ',num2str(preparation(w)),' dt= ', num2str(discovery(dt)) ] )
+title([num2str(densityBL(D_BL)),' Blocker Density w= ',num2str(preparation(w)),' dt= ', num2str(discovery(dt)) ] )
 
-D_BL =1;
+D_BL =2;
 w=2;
 figure()
 
@@ -36,10 +44,14 @@ hold on;
 grid on;
 semilogy(lambda_BS,P_OS(:,K_1,w,dt,D_BL),'-b+')
 
+% K_2=2;
+% semilogy(lambda_BS, reshape(final_results(dt,w,:,K_2,D_BL),[],1),'-^r')
+% semilogy(lambda_BS,P_OS(:,K_2,w,dt,D_BL),'-b^')
+
 K_2=1;
 semilogy(lambda_BS, reshape(final_results(dt,w,:,K_2,D_BL),[],1),'-vr')
 semilogy(lambda_BS,P_OS(:,K_2,w,dt,D_BL),'-bv')
 
 legend(['Simulation K=',num2str(K_1)],['Theory K=',num2str(K_1)],['Simulation K=',num2str(K_2)],['Theory K=',num2str(K_2)])
 
-title(['Low Blocker Density w= ',num2str(preparation(w)),' dt= ', num2str(discovery(dt)) ] )
+title([num2str(densityBL(D_BL)),' Blocker Density w= ',num2str(preparation(w)),' dt= ', num2str(discovery(dt)) ] )
