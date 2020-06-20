d13dfe21d199f6c1a964279074e6dbadaeb5255f
Thanos <apk337@nyu.edu>
2020-06-20 11:39:28 -0400

    Prof idea about duration

--- Continuous_Chain/oos_duration_linear_eqn.m
+++ Continuous_Chain/oos_duration_linear_eqn.m
@@ -79,5 +79,74 @@ P_OS = P_OS./(1-exp(-self_blockage*pi*R^2*lambda_BS));
 T_OS = T_OS./(1-exp(-self_blockage*pi*R^2*lambda_BS));
 save('T_OS','T_OS');
 
+%% Professor's idea, duration = 1/(rho * omega)
+
+clear;
+
+V = 1; %velocity of blocker m/s
+hb = 1.8; %height blocker
+hr = 1.4; %height receiver (UE)
+ht = 5; %height transmitter (BS)
+frac = (hb-hr)/(ht-hr);
+mu = 2; %Expected bloc dur =1/mu sec
+u = mu;
+R = 100; %m Radius
+lambda_B = [0.01 0.1];
+C = 2*V.*lambda_B*frac/pi;
+
+
+lambda_BS = [200 300 400 500]*10^(-6); %densityBS
+density_limits = [60,60,60,60];
+K_list = [1,2,3,4];                      % Degree of Connectivity
+w_list = 1000./[5,10,15,20,100]; %1000./[10,15,25,30,70,200,1000];      %Connection establishment times
+dt_list = 1000./[1,5,20,200,1000];   %Discovery Times
+a_list = C.*2*R/3;                       %Blocker Arrivals
+self_blockage = 5/6;
+
+
+P_OS = zeros(length(lambda_BS),length(K_list),length(w_list),length(dt_list),length(a_list));
+T_OS = zeros(length(lambda_BS),length(K_list),length(w_list),length(dt_list),length(a_list));
+
+
+for indBS=1:length(lambda_BS)
+    M_max = density_limits(indBS);
+    for M = 1:M_max
+        P_M =  exp(-1*lambda_BS(indBS)*self_blockage*pi*R^2) *...
+            (lambda_BS(indBS)*self_blockage*pi*R^2)^(M)/factorial(M);
+        for indK = 1:length(K_list)
+            K = K_list(indK);
+            for indW = 1:length(w_list)
+                w = w_list(indW);
+                for indDt = 1:length(dt_list)
+                    dt = dt_list(indDt);
+                    u = 1/(1/mu + 1/dt);
+                    for indA = 1:length(a_list)
+                        alpha = a_list(indA);
+                        X = P_out_mxk(M,K,alpha,u,w);
+                        sum1 = 0;
+                        for i=1:M
+                            rho(i) = min(i,K);
+                            sum1 = sum1 + min(i,K)*w*X(i+1);
+                        end 
+                        T_OS(indBS,indK,indW,indDt,indA) = T_OS(indBS,indK,indW,indDt,indA) + ...
+                            P_M * 1/sum1/sum(X);
+                        
+
+                        P_OS(indBS,indK,indW,indDt,indA) = P_OS(indBS,indK,indW,indDt,indA) + P_M * sum(X);
+%                         P_OS(indBS,indK,indW,indDt,indA) = sum(X([chain_states.right]==0));
+                        clearvars chain_states
+                    end
+                end
+            end
+        end
+    end
+end
+P_OS = P_OS./(1-exp(-self_blockage*pi*R^2*lambda_BS));
+T_OS = T_OS./(1-exp(-self_blockage*pi*R^2*lambda_BS));
+save('T_OS','T_OS');
+
+
+
+
 
 
